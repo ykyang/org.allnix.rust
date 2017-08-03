2e1da4acbd03adef447acd49510df095e72a5b28
Yi-Kun Yang <ykyang@gmail.com>
2017-08-03 18:55:55 -0500

    rust-lang: 12.6: Writing to stderr ...

--- rust-lang/greprs/src/main.rs
+++ rust-lang/greprs/src/main.rs
@@ -2,14 +2,21 @@ extern crate greprs;
 
 use std::env;
 use std::process;
+use std::io::prelude::Write;
 
 use greprs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // > stderr
+    let mut cerr = std::io::stderr();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        writeln!(
+            &mut cerr,
+            "Problem parsing arguments: {}", err
+        ).expect("Could not write to stderr");
+
         process::exit(1);
     });
 
@@ -17,7 +24,11 @@ fn main() {
 //    println!("In file {}", config.filename);
 
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        writeln!(
+            &mut cerr,
+            "Application error: {}", e
+        ).expect("Could not write to stderr");
+
         process::exit(1);
     }
 }
