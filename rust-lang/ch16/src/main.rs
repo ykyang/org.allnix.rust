b4a9646a246c3db1578ebd6437ed515777d1dab5
Yi-Kun Yang <ykyang@gmail.com>
2017-08-04 20:42:39 -0500

    rust-lang: 16.2: Message Passing

--- rust-lang/ch16/src/main.rs
+++ rust-lang/ch16/src/main.rs
@@ -1,19 +1,26 @@
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
-    let v = vec![1,2,3];
+    //    ch16_1();
+    ch16_2();
+}
 
-    let handle = thread::spawn(move||{
+fn ch16_1() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
         println!("Here is a vector: {:?}", v);
     });
 
-//    drop(v);
-    handle.join();
+    //    drop(v);
+    //    handle.join();
 
-    let handle = thread::spawn(||{
-       for i in 1..10 {
-           println!("Hi number {} from the spawned thread!", i);
-       }
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hi number {} from the spawned thread!", i);
+        }
     });
 
     for i in 1..5 {
@@ -21,4 +28,40 @@ fn main() {
     }
 
     handle.join();
+}
+
+fn ch16_2() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx2 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
